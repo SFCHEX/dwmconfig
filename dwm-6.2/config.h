aecac74144595db628d651a071b2331b69c2b305
CHEX <52354962+SFCHEX@users.noreply.github.com>
2021-05-20 19:38:51 +0200

    Add files via upload

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -77,7 +77,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h" ,"20", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34","-C","#323845@257", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *bravecmd[]  = { "brave", NULL };
 
